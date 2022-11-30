58ca603e3457e2452a92da34d5b38a702d8b54a0
AvE-o <tt623457092@gmail.com>
2022-11-30 18:04:14 -0500

    Not much progress need do more research

--- demo/AM_blocks_from_microphone1.py
+++ demo/AM_blocks_from_microphone1.py
@@ -1,21 +1,15 @@
-#1 create a filter that change the intake voice
+# AM_from_microphone.py
+# Record audio and play it with amplitude modulation. 
+# This implementation:
+#   uses blocking, 
+#   corrects for block-to-block angle mismatch,
+#   assumes mono channel
+# Original by Gerald Schuller, 2013
+
 
 import pyaudio
 import struct
 import math
-import wave
-import numpy as np
-from scipy.signal import spectrogram, windows
-
-def clip16( x ):    
-    # Clipping for 16 bits
-    if x > 32767:
-        x = 32767
-    elif x < -32768:
-        x = -32768
-    else:
-        x = x        
-    return (x)
 
 # f0 = 0      # Normal audio
 f0 = 400    # Modulation frequency (Hz)
@@ -24,6 +18,7 @@ BLOCKLEN = 64      # Number of frames per block
 WIDTH = 2           # Number of bytes per signal value
 CHANNELS = 1        # mono
 RATE = 32000        # Frame rate (frames/second)
+RECORD_SECONDS = 5
 
 p = pyaudio.PyAudio()
 
@@ -41,23 +36,29 @@ output_block = BLOCKLEN * [0]
 om = 2*math.pi*f0/RATE
 theta = 0
 
-while (True):
+# Number of blocks to run for
+num_blocks = int(RATE / BLOCKLEN * RECORD_SECONDS)
 
-    input_bytes = stream.read(BLOCKLEN, exception_on_overflow = False)   
+print('* Recording for %.3f seconds' % RECORD_SECONDS)
+
+# Start loop
+for i in range(0, num_blocks):
+
+    # Get frames from audio input stream
+    # input_bytes = stream.read(BLOCKLEN)       # BLOCKLEN = number of frames read
+    input_bytes = stream.read(BLOCKLEN, exception_on_overflow = False)   # BLOCKLEN = number of frames read
+
+    # Convert binary data to tuple of numbers
     input_tuple = struct.unpack('h' * BLOCKLEN, input_bytes)
    
+    # Go through block
     for n in range(0, BLOCKLEN):
-
+        # No processing:
+        # output_block[n] = input_tuple[n]  
+        # OR
+        # Amplitude modulation:
         theta = theta + om
-        # output_block[n] = clip16(int( input_tuple[n] * math.cos(theta) ))
-        output_block[n] = clip16(int(math.sin(2*math.pi*f0/RATE)))
-        # output_block[n] = clip16(int(np.sin(2*np.pi*f0/RATE)))
-    
-    # f, t, sxx = spectrogram(np.array(input_tuple), fs=f0, nfft=BLOCKLEN, noverlap=None)
-    # output_block = sxx
-    # x = np.fft.rfft(input_tuple)
-    # output_block = np.fft.irfft(x)
-    # output_block = output_block.astype(int)
+        output_block[n] = int( input_tuple[n] * math.cos(theta) )
 
     # keep theta betwen -pi and pi
     while theta > math.pi:
